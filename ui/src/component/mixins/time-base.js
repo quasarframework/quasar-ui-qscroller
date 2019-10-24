a6195b3220bd172d7799b3390649758a6897ae5a
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-10-24 17:31:19 -0600

    chore: add common method

--- ui/src/component/mixins/time-base.js
+++ ui/src/component/mixins/time-base.js
@@ -14,5 +14,19 @@ export default {
       default: () => ['AM', 'PM'],
       validator: v => Array.isArray(v) && v.length === 2 && typeof v[0] === 'string' && typeof v[1] === 'string'
     }
+  },
+
+  methods: {
+    isValidTime (time) {
+      let parts = time.split(':')
+      if (parts.length === 2) {
+        let hour = parseInt(parts[0])
+        let minute = parseInt(parts[1])
+        if (hour >= 0 && hour < 24 && minute >= 0 && minute < 60) {
+          return true
+        }
+      }
+      return false
+    }
   }
 }
