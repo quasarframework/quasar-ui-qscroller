d6c7f882851eeeef2e394b1a9fbf57ffdd78b020
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-10-25 07:59:04 -0600

    feat(dev): time-range input example

---
+++ ui/dev/src/pages/3-QTimeRangeScroller-Value-Types.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="q-pa-md row q-gutter-sm full-width">
+    <div class="row full-width">
+      <div class="text-h4">QTimeRangeScroller</div>
+    </div>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-footer, value as string
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="valueStr"
+          view="time-range"
+          no-footer
+          style="max-width: 200px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-footer, value as object
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="valueObj"
+          view="time-range"
+          no-footer
+          style="max-width: 200px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-footer, value as array
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="valueArr"
+          view="time-range"
+          no-footer
+          style="max-width: 200px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-footer, value as Date
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="valueDate"
+          view="time-range"
+          no-footer
+          style="max-width: 200px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      valueStr: ['09:00','09:00'],
+      valueObj: [{ hour: '09', minute: '10' },{ hour: '09', minute: '10' }],
+      valueArr: [[ '09', '20'],[ '09', '20']],
+      valueDate: [new Date(), new Date()]
+    }
+  },
+
+  computed: {
+    scrollerPopupStyle150 () {
+      if (this.$q.screen.lt.sm) {
+        return {
+          width: '90vw',
+          height: '90vh'
+        }
+      } else {
+        return {
+          maxHeight: '200px',
+          height: '200px',
+          width: '150px'
+        }
+      }
+    }
+  },
+
+  watch: {
+    valueStr (val) {
+      console.log('string:', val)
+    },
+    valueObj (val) {
+      console.log('object:', JSON.stringify(val))
+    },
+    valueArr (val) {
+      console.log('array:', JSON.stringify(val))
+    },
+    valueDate (val) {
+      console.log('date:', val)
+    }
+  }
+}
+</script>
