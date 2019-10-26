08b8c6c2b27c60b62d06ebe0c8020f99cba150b9
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-10-26 11:10:01 -0600

    chore(dev): more date-range examples

---
+++ ui/dev/src/pages/5-QDateRangeScroller-Borders.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="q-pa-md row q-gutter-sm full-width">
+    <div class="row full-width">
+      <div class="text-h4">QTimeRangeScroller</div>
+    </div>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        show-vertical-bar, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          show-vertical-bar
+          style="max-width: 280px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        show-vertical-bar, bar-color, hour12, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          show-vertical-bar
+          bar-color="primary"
+          style="max-width: 280px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        show-vertical-bar, bar-color, border-color, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          show-vertical-bar
+          bar-color="primary"
+          border-color="primary"
+          hour12
+          style="max-width: 280px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
---
+++ ui/dev/src/pages/5-QDateRangeScroller-Intervals.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="q-pa-md row q-gutter-sm full-width">
+    <div class="row full-width">
+      <div class="text-h4">QDateRangeScroller</div>
+    </div>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-years, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          start-no-years
+          end-no-years
+          style="max-width: 280px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-months, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          start-no-months
+          end-no-months
+          style="max-width: 280px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-days, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          start-no-days
+          end-no-days
+          style="max-width: 300px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-months, no-days, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          start-no-months
+          end-no-months
+          start-no-days
+          end-no-days
+          style="max-width: 300px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-years, no-days, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          start-no-years
+          end-no-years
+          start-no-days
+          end-no-days
+          style="max-width: 300px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-years, no-months, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          start-no-years
+          end-no-years
+          start-no-months
+          end-no-months
+          style="max-width: 300px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
