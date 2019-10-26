e1dd341613d58abe474b17485fa43be9417858a3
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-10-26 10:33:43 -0600

    feat(ui): new base mixin for date scroller

---
+++ ui/src/component/mixins/date-base.js
@@ -0,0 +1,25 @@
+import props from '../utils/props'
+
+export default {
+  name: 'DateBase',
+
+  props: {
+    ...props.common,
+    ...props.base,
+    ...props.locale
+  },
+
+  data () {
+    return {
+      //
+    }
+  },
+
+  computed: {
+    //
+  },
+
+  methods: {
+    //
+  }
+}
