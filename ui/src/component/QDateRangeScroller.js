512694159b772eb7b7caec45ddd990f211a0d744
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-10-26 10:49:23 -0600

    feat(date-range): refactored

--- ui/src/component/QDateRangeScroller.js
+++ ui/src/component/QDateRangeScroller.js
@@ -1,29 +1,39 @@
 // Mixins
-import DateTimeBase from './mixins/datetime-base'
+import DateBase from './mixins/date-base'
+import { QColorizeMixin } from 'q-colorize-mixin'
 import QDateScroller from './QDateScroller'
 
 // Util
 import props from './utils/props'
 import { QBtn, QResizeObserver } from 'quasar'
 import {
-  daysInMonth
+  getDayIdentifier,
+  parsed,
+  parseDate,
+  getDate,
+  getTime,
+  daysInMonth,
+  padNumber
 } from './utils/timestamp'
 
 /* @vue/component */
 export default {
   name: `QDateRangeScroller`,
 
-  mixins: [DateTimeBase],
+  mixins: [DateBase, QColorizeMixin],
 
   props: {
     ...props.common,
-    ...props.dateRange
+    ...props.dateRange,
+    ...props.verticalBar
   },
 
   data () {
     return {
-      headerFooterHeight: 100,
+      headerHeight: 50,
+      footerHeight: 50,
       bodyHeight: 100,
+      height: 0,
       startDate: '',
       endDate: '',
       type: null
@@ -31,17 +41,18 @@ export default {
   },
 
   mounted () {
-    if (!(Array.isArray(this.value) || typeof this.value === 'string')) {
-      console.error('QDateRangeScroller - value (v-model) must to be an array of dates')
-    }
-    if (Array.isArray(this.value) && this.value.length !== 2) {
-      console.error('QDateRangeScroller - value (v-model) must contain 2 array elements')
-    }
     this.splitDate()
     this.adjustBodyHeight()
   },
 
   computed: {
+    style () {
+      let style = {}
+      style['--scroller-border-color'] = this.calculateColor(this.borderColor)
+      style['--scroller-bar-color'] = this.calculateColor(this.barColor)
+      return style
+    },
+
     displayDate () {
       if (this.startDate !== void 0 && this.endDate !== void 0) {
         if (this.$refs.startDate && this.$refs.endDate) {
@@ -49,7 +60,7 @@ export default {
         }
         return `${this.startDate}${this.displaySeparator}${this.endDate}`
       }
-      return ''
+      return `${this.displaySeparator}`
     }
   },
 
@@ -81,13 +92,77 @@ export default {
 
   methods: {
     emitValue () {
-      if (this.type === 'array') {
-        this.$emit('input', [
-          this.startDate, this.endDate
-        ])
-      } else if (this.type === 'string') {
-        this.$emit('input', `${this.startDate}${this.displaySeparator}${this.endDate}`)
+      let startParts, endParts,  start, end
+      switch (this.type) {
+        case 'date':
+          start = parseDate(new Date())
+          end = parseDate(new Date())
+          startParts = this.startDate.split('-')
+          endParts = this.endDate.split('-')
+          start.year = parseInt(startParts[0])
+          start.month = parseInt(startParts[1])
+          start.day = parseInt(startParts[2])
+          end.year = parseInt(endParts[0])
+          end.month = parseInt(endParts[1])
+          end.day = parseInt(endParts[2])
+          this.$emit('input', [ getDateObject(this.start), getDateObject(this.end) ])
+          return
+        case 'array':
+          startParts = this.startDate.split('-')
+          endParts = this.endDate.split('-')
+          this.$emit('input', [
+            [ parseInt(startParts[0]), parseInt(startParts[1]), parseInt(startParts[2]) ],[ parseInt(endParts[0]), parseInt(endParts[1]), parseInt(endParts[2]) ]
+          ])
+          return
+        case 'object':
+          startParts = this.startDate.split('-')
+          endParts = this.endDate.split('-')
+          this.$emit('input', [
+            { year: parseInt(startParts[0]), month: parseInt(startParts[1]), day: parseInt(startParts[2]) }, { year: parseInt(endParts[0]), month: parseInt(endParts[1]), day: parseInt(endParts[2]) }
+          ])
+
+          return
+        case 'string':
+          this.$emit('input', [
+            this.startDate, this.endDate
+          ])
+          return
       }
+      if (this.startDate !== '' && this.endDate !== '') {
+        if (this.type === 'array') {
+          this.$emit('input', [
+            this.startDate, this.endDate
+          ])
+        } else if (this.type === 'object') {
+          this.$emit('input', `{ start: ${this.startDate}, end: ${this.endDate} }`)
+        } else { // if (this.type === 'string') {
+          this.$emit('input', `${this.startDate}${this.displaySeparator}${this.endDate}`)
+        }
+      }
+    },
+
+    isValidRange () {
+      // check if endDate is > startDate
+      if (this.startDate && this.endDate) {
+        let start = parseDate(new Date())
+        let end = parseDate(new Date())
+        const startParts = this.startDate.split('-')
+        const endParts = this.endDate.split('-')
+        start.year = parseInt(startParts[0])
+        start.month = parseInt(startParts[1])
+        start.day = parseInt(startParts[2])
+        end.year = parseInt(endParts[0])
+        end.month = parseInt(endParts[1])
+        end.day = parseInt(endParts[2])
+        const startDate = getDayIdentifier(start)
+        const endDate = getDayIdentifier(end)
+        if (endDate >= startDate) {
+          return true
+        }
+        return false
+      }
+      // until everything is mounted, just return true
+      return true
     },
 
     onResize ({ height }) {
@@ -95,43 +170,85 @@ export default {
     },
 
     adjustBodyHeight () {
-      if (this.height !== void 0) {
-        this.bodyHeight = this.height
-      } else {
-        this.$nextTick(() => {
-          let headerHeight = this.noHeader ? 0 : this.$refs.header ? this.$refs.header.clientHeight : 0
-          let footerHeight = this.noFooter ? 0 : this.$refs.footer ? this.$refs.footer.clientHeight : 0
-          this.headerFooterHeight = headerHeight + footerHeight
-          const parentHeight = this.$refs.scroller ? window.getComputedStyle(this.$refs.scroller, null).getPropertyValue('height') : 0
-          this.bodyHeight = parseInt(parentHeight) - this.headerFooterHeight
-        })
-      }
+      let self = this
+      this.$nextTick(() => {
+        this.headerHeight = this.noHeader === true ? 0 : this.$refs.header ? parseInt(window.getComputedStyle(this.$refs.header, null).getPropertyValue('height')) : 0
+        this.footerHeight = this.noFooter === true ? 0 : this.$refs.footer ? parseInt(window.getComputedStyle(this.$refs.footer, null).getPropertyValue('height')) : 0
+        this.height = parseInt(window.getComputedStyle(self.$el, null).getPropertyValue('height'))
+        this.bodyHeight = this.height - this.headerHeight - this.footerHeight
+      })
     },
 
     splitDate () {
-      if (Array.isArray(this.value)) {
-        const start = this.value[0].trim()
-        const end = this.value[1].trim()
-        if (this.isValidDate(start) && this.isValidDate(end)) {
-          this.startDate = start
-          this.endDate = end
-          this.type = 'array'
-          return
-        }
-      } else {
-        const parts = this.value.split(this.displaySeparator)
-        if (parts.length === 2) {
-          const start = parts[0].trim()
-          const end = parts[1].trim()
+      // QDateRangeScroller takes an array of Date, Object, Array or String
+      let start, end, now
+      let type = Object.prototype.toString.call(this.value)
+      // use first item to determine type; convert to string for sub-components
+      type = Object.prototype.toString.call(this.value[0])
+      switch (type) {
+        case '[object Date]':
+          this.type = 'date'
+          start = parseDate(this.value[0])
+          start = getDate(start) + ' ' + getTime(start)
+          start = getDate(parsed(start))
+          end = parseDate(this.value[1])
+          end = getDate(end) + ' ' + getTime(end)
+          end = getDate(parsed(end))
           if (this.isValidDate(start) && this.isValidDate(end)) {
             this.startDate = start
             this.endDate = end
-            this.type = 'string'
-            return
+          } else {
+            console.error('QDateRangeScroller: invalid start or end dates')
           }
-        }
+          return
+        case '[object Array]':
+          this.type = 'array'
+          start = padNumber(parseInt(this.value[0][0]), 2) + '-' + padNumber(parseInt(this.value[0][1]), 2) + '-' + padNumber(parseInt(this.value[0][2]), 2)
+          end = padNumber(parseInt(this.value[1][0]), 2) + '-' + padNumber(parseInt(this.value[1][1]), 2) + '-' + padNumber(parseInt(this.value[1][2]), 2)
+          if (this.isValidDate(start) && this.isValidDate(end)) {
+            this.startDate = start
+            this.endDate = end
+          } else {
+            console.error('QDateRangeScroller: invalid start or end dates')
+          }
+          return
+        case '[object Object]':
+          this.type = 'object'
+          start = padNumber(parseInt(this.value[0].year), 2) + '-' + padNumber(parseInt(this.value[0].month), 2) + '-' + padNumber(parseInt(this.value[0].day), 2)
+          end = padNumber(parseInt(this.value[1].year), 2) + '-' + padNumber(parseInt(this.value[1].month), 2) + '-' + padNumber(parseInt(this.value[1].minute), 2)
+          if (this.isValidDate(start) && this.isValidDate(end)) {
+            this.startDate = start
+            this.endDate = end
+          } else {
+            console.error('QDateRangeScroller: invalid start or end dates')
+          }
+          return
+        case '[object String]':
+          this.type = 'string'
+          start = this.value[0]
+          end = this.value[1]
+          if (this.isValidDate(start) && this.isValidDate(end)) {
+            this.startDate = start
+            this.endDate = end
+          } else {
+            console.error('QDateRangeScroller: invalid start or end dates')
+          }
+          return
+        case '[object Undefined]':
+          // if nothing is provided, then use current time as array of strings
+          this.type = 'string'
+          now = new Date()
+          now = parseDate(now)
+          start = getDate(now) + ' ' + getTime(now)
+          start = getDate(parsed(start))
+          end = start
+          if (this.isValidDate(start) && this.isValidDate(end)) {
+            this.startDate = start
+            this.endDate = end
+          } else {
+            console.error('QDateRangeScroller: invalid start or end dates')
+          }
       }
-      console.error(`QDateRangeScroller: invalid date format - '${this.value}'`)
     },
 
     isValidDate (date) {
@@ -153,12 +270,14 @@ export default {
       const slot = this.$scopedSlots.header
       return h('div', {
         ref: 'header',
-        staticClass: (this.dense ? 'q-scroller__header--dense' : 'q-scroller__header') + ' flex justify-around items-center full-width ellipsis q-pa-xs',
+        staticClass: (this.dense ? 'q-scroller__header--dense' : 'q-scroller__header') + ' flex justify-around items-center full-width q-pa-xs',
         class: {
           'shadow-20': this.noShadow === false
-        }
+        },
       }, slot ? slot([this.$refs.startDate.getTimestamp(), this.$refs.endDate.getTimestamp()]) : [
-        this.displayDate
+        h('span', {
+          staticClass: 'ellipsis'
+        }, this.displayDate)
       ])
     },
 
@@ -169,21 +288,19 @@ export default {
         props: {
           value: this.startDate,
           locale: this.locale,
-          showVerticalBar: true,
           barColor: this.barColor,
+          textColor: this.textColor,
           color: this.color,
-          backgroundColor: this.backgroundColor,
+          innerTextColor: this.innerTextColor,
           innerColor: this.innerColor,
-          innerBackgroundColor: this.innerBackgroundColor,
+          disabledTextColor: this.disabledTextColor,
           dense: this.dense,
           disable: this.disable,
           noBorder: true,
           noHeader: true,
           noFooter: true,
-          minDate: this.startMinDate,
-          maxDate: this.startMaxDate,
-          dates: this.startDates,
-          disabledDate: this.startDisabledDates,
+          startDate: this.startStartDate,
+          endDate: this.startEndDate,
           disabledYears: this.startDisabledYears,
           disabledMonths: this.startDisabledMonths,
           disabledDays: this.startDisabledDays,
@@ -194,8 +311,10 @@ export default {
           showWeekdayLabel: this.startShowWeekdayLabel,
           noDays: this.startNoDays,
           noMonths: this.startNoMonths,
-          noYears: this.startNoYears,
-          height: this.bodyHeight
+          noYears: this.startNoYears
+        },
+        class: {
+          'q-scroller__vertical-bar': this.showVerticalBar === true
         },
         on: {
           input: v => { this.startDate = v }
@@ -211,19 +330,18 @@ export default {
           value: this.endDate,
           locale: this.locale,
           barColor: this.barColor,
+          textColor: this.textColor,
           color: this.color,
-          backgroundColor: this.backgroundColor,
-          innerColor: this.innerColor,
-          innerBackgroundColor: this.innerBackgroundColor,
+          innerTextColor: this.isValidRange() ? this.innerTextColor : this.errorTextColor,
+          innerColor: this.isValidRange() ? this.innerColor : this.errorColor,
+          disabledTextColor: this.disabledTextColor,
           dense: this.dense,
           disable: this.disable,
           noBorder: true,
           noHeader: true,
           noFooter: true,
-          minDate: this.endMinDate,
-          maxDate: this.endMaxDate,
-          dates: this.endDates,
-          disabledDate: this.endDisabledDates,
+          startDate: this.endStartDate,
+          startDate: this.endStartDate,
           disabledYears: this.endDisabledYears,
           disabledMonths: this.endDisabledMonths,
           disabledDays: this.endDisabledDays,
@@ -234,8 +352,7 @@ export default {
           showWeekdayLabel: this.endShowWeekdayLabel,
           noDays: this.endNoDays,
           noMonths: this.endNoMonths,
-          noYears: this.endNoYears,
-          height: this.bodyHeight
+          noYears: this.endNoYears
         },
         on: {
           input: v => { this.endDate = v }
@@ -286,7 +403,7 @@ export default {
         staticClass: (this.dense ? 'q-scroller__footer--dense' : 'q-scroller__footer') + ' flex justify-around items-center full-width q-pa-xs',
         class: {
           'shadow-up-20': this.noShadow === false
-        }
+        },
       }, slot || [
         this.__renderFooterButton(h)
       ])
@@ -294,7 +411,7 @@ export default {
   },
 
   render (h) {
-    const child = [
+    const resize = [
       h(QResizeObserver, {
         props: { debounce: 0 },
         on: { resize: this.onResize }
@@ -309,12 +426,8 @@ export default {
         'rounded-borders': this.roundedBorders === true,
         'q-scroller__border': this.noBorder !== true
       },
-      style: {
-        '--scroller-border-color': this.borderColor,
-        '--scroller-bar-color': this.barColor,
-        'overflow': 'hidden'
-      }
-    }), child.concat([
+      style: this.style
+    }), resize.concat([
       this.__renderHeader(h),
       this.__renderBody(h),
       this.__renderFooter(h)
