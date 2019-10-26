07039892c12a4086c2f2cc004c1d9e7463174472
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-10-26 10:50:01 -0600

    chore(dev): start of date-range examples

---
+++ ui/dev/src/pages/5-QDateRangeScroller-Basic.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="q-pa-md row q-gutter-sm full-width">
+    <div class="row full-width">
+      <div class="text-h4">QDateRangeScroller</div>
+    </div>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-header, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-header
+          no-footer
+          style="max-width: 300px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          style="max-width: 300px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-shadow, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          no-shadow
+          style="max-width: 300px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        no-border, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          no-border
+          style="max-width: 300px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        rounded-borders, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          rounded-borders
+          style="max-width: 300px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        dense, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          dense
+          style="max-width: 250px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        disabled, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          disable
+          style="max-width: 300px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        dense, disabled, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          dense
+          disable
+          style="max-width: 250px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+    <q-card class="q-pa-md">
+      <q-card-section class="col-12">
+        dense, disabled, no-footer
+      </q-card-section>
+      <q-card-section class="col-12">
+        <q-scroller
+          v-model="value"
+          view="date-range"
+          no-footer
+          dense
+          disable
+          disabled
+          style="max-width: 250px; height: 200px;"
+        ></q-scroller>
+      </q-card-section>
+    </q-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
