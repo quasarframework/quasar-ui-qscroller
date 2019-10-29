c727655dc2c4dd5899093772db8715f45a676a11
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-10-29 13:33:19 -0600

    chore(ui): add api as exportable

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,18 +1,17 @@
 import { version } from '../package.json'
-
 import Component from './component/QScroller'
+import Api from './component/QScroller.json'
 
 export {
   version,
-
-  Component
-
+  Component,
+  Api
 }
 
 export default {
   version,
-
   Component,
+  Api,
 
   install (Vue) {
     Vue.component(Component.name, Component)
