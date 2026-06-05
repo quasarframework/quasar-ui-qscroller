b83161c61853b386aede2fcd39775b8201e8acb3
Jeff <galbraith64@gmail.com>
2026-06-05 06:54:46 -0600

    refine landing preview label

--- packages/docs/src/components/page-parts/LandingPageContent.vue
+++ packages/docs/src/components/page-parts/LandingPageContent.vue
@@ -82,7 +82,7 @@
           <div class="preview-panel">
             <div class="preview-panel__header">
               <span class="preview-panel__kicker">Focused Inputs</span>
-              <span class="preview-panel__note">Docs and app-ready</span>
+              <span class="preview-panel__note">Focused picker flows</span>
             </div>
 
             <div class="preview-panel__body">
